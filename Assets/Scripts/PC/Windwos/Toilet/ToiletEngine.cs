0251b516276a4f43f395c028fe8c563cc31bb80c
Daniel Wemmer <daniel.wemmer.dw@gmail.com>
2016-02-12 16:20:05 +0100

    Add Project
    Added Project

---
+++ Assets/Scripts/PC/Windwos/Toilet/ToiletEngine.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToiletEngine : MonoBehaviour {
+
+	public int maxClick;
+	private int clicked;
+
+	public GameObject Devil;
+	public GameObject Good;
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Wenn Maus_Links drücken, dann...
+		if (Input.GetMouseButtonDown (0)) {
+			clicked = clicked + 1;
+
+			if (clicked == maxClick) {
+				Devil.SetActive (true);
+			}
+
+			if(clicked > maxClick){
+				Devil.SetActive (false);
+				Good.SetActive (true);
+			}
+		}
+	}
+}
